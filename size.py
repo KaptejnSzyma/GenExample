b20ff5ff1257263165b7be9095ce1d2972d2c084
Szymek <szymon.rzadca@gmail.com>
2018-07-01 15:53:03 +0200

    defining my_range function and testing it

--- size.py
+++ size.py
@@ -1,7 +1,15 @@
 import sys
 
-big_range = range(1000)
 
+def my_range(n: int):
+    start = 0
+    while start < n:
+        yield start
+        start += 1
+
+
+# big_range = range(5)
+big_range = my_range(5)
 print("big_range is {} bytes".format(sys.getsizeof(big_range)))
 
 # create a list containing all the values in big_range
@@ -10,3 +18,5 @@ for val in big_range:
     big_list.append(val)
 
 print("big_list is {} bytes".format(sys.getsizeof(big_list)))
+print(big_range)
+print(big_list)
